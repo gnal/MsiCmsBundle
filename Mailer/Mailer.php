d1732fcbe993fe845d00a7d21b314e496f7859d0
gnal <qwertypianox@gmail.com>
2013-09-23 16:34:36 -0400

    fix

--- Mailer/Mailer.php
+++ Mailer/Mailer.php
@@ -17,36 +17,38 @@ class Mailer
 
     public function sendEmail($name, $data = null, $toWho = null)
     {
-        $email = $this->emailManager->find(
+        $emails = $this->emailManager->findAll(
             ['a.name' => $name]
         );
 
-        if ($data) {
-            $body = preg_replace_callback(
-                '@#[a-zA-z0-9]+#@',
-                function ($matches) use ($data) {
-                    $code = str_replace('#', '', $matches[0]);
-                    $getter = 'get'.ucfirst(strtolower($code));
+        foreach ($emails as $email) {
+            if ($data) {
+                $body = preg_replace_callback(
+                    '@#[a-zA-z0-9]+#@',
+                    function ($matches) use ($data) {
+                        $code = str_replace('#', '', $matches[0]);
+                        $getter = 'get'.ucfirst(strtolower($code));
 
-                    return $data->$getter();
-                },
-                $email->getTranslation()->getBody()
+                        return $data->$getter();
+                    },
+                    $email->getTranslation()->getBody()
+                );
+            } else {
+                $body = $email->getTranslation()->getBody();
+            }
+
+            $rendered = $this->templating->render('MsiCmsBundle:Email:base.html.twig', [
+                'subject' => $email->getTranslation()->getSubject(),
+                'body' => $body,
+            ]);
+
+            $this->send(
+                $email->getFromWho(),
+                $toWho ?: $email->getToWho(),
+                $email->getTranslation()->getSubject(),
+                $rendered
             );
-        } else {
-            $body = $email->getTranslation()->getBody();
         }
-
-        $rendered = $this->templating->render('MsiCmsBundle:Email:base.html.twig', [
-            'subject' => $email->getTranslation()->getSubject(),
-            'body' => $body,
-        ]);
-
-        $this->send(
-            $email->getFromWho(),
-            $toWho ?: $email->getToWho(),
-            $email->getTranslation()->getSubject(),
-            $rendered
-        );
     }
 
     protected function send($fromWho, $toWho, $subject, $body)
